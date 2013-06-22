690722dffc9061f307a32715f8ce954f4f7eb2dc
Martin Angers <martin.n.angers@gmail.com>
2013-06-21 22:27:32 -0400

    add Printf stdlib func

--- runtime/stdlib/fmt.go
+++ runtime/stdlib/fmt.go
@@ -11,9 +11,10 @@ var Fmt map[string]runtime.NativeFunc
 func init() {
 	Fmt = make(map[string]runtime.NativeFunc, 1)
 	Fmt["fmt.Println"] = fmt_Println
+	Fmt["fmt.Printf"] = fmt_Printf
 }
 
-func fmt_Println(s runtime.Streams, args ...runtime.Val) runtime.Val {
+func toNative(args []runtime.Val) []interface{} {
 	var ifs []interface{}
 
 	if len(args) > 0 {
@@ -22,7 +23,25 @@ func fmt_Println(s runtime.Streams, args ...runtime.Val) runtime.Val {
 			ifs[i] = v.Native()
 		}
 	}
+	return ifs
+}
+
+func fmt_Println(s runtime.Streams, args ...runtime.Val) runtime.Val {
+	ifs := toNative(args)
 	n, err := fmt.Fprintln(s.Stdout(), ifs...)
+	if err != nil {
+		panic(err)
+	}
+	return runtime.Int(n)
+}
+
+func fmt_Printf(s runtime.Streams, args ...runtime.Val) runtime.Val {
+	var ft string
+	if len(args) > 0 {
+		ft = args[0].String()
+	}
+	ifs := toNative(args[1:])
+	n, err := fmt.Fprintf(s.Stdout(), ft, ifs)
 	if err != nil {
 		panic(err)
 	}
