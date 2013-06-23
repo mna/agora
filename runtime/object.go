df8e40dd3649b2daeca324a15019446eff62beb1
Martin Angers <martin.n.angers@gmail.com>
2013-06-23 16:26:32 -0400

    more work on Object

--- runtime/object.go
+++ runtime/object.go
@@ -8,8 +8,15 @@ var (
 	ErrInvalidConvObjToInt    = errors.New("cannot convert Object to Int")
 	ErrInvalidConvObjToFloat  = errors.New("cannot convert Object to Float")
 	ErrInvalidConvObjToString = errors.New("cannot convert Object to String")
-	ErrInvalidConvObjToBool   = errors.New("cannot convert Object to Bool")
 	ErrInvalidConvObjToNative = errors.New("cannot convert Object to Native")
+
+	ErrInvalidOpAddOnObj = errors.New("cannot apply Add on this Object")
+	ErrInvalidOpSubOnObj = errors.New("cannot apply Sub on this Object")
+	ErrInvalidOpMulOnObj = errors.New("cannot apply Mul on this Object")
+	ErrInvalidOpDivOnObj = errors.New("cannot apply Div on this Object")
+	ErrInvalidOpPowOnObj = errors.New("cannot apply Pow on this Object")
+	ErrInvalidOpModOnObj = errors.New("cannot apply Mod on this Object")
+	ErrInvalidOpUnmOnObj = errors.New("cannot apply Unm on this Object")
 )
 
 type Object struct {
@@ -41,12 +48,29 @@ func (ø Object) Bool() bool {
 	if b, ok := ø["__toBool"]; ok {
 		return b
 	}
-	panic(ErrInvalidConvObjToBool)
+	// If __toBool is not defined, object returns true (since it is not nil)
+	return true
 }
 
 func (ø Object) Native() interface{} {
 	if o, ok := ø["__toNative"]; ok {
-		return o
+		return o // TODO : Need to call the method, not return it!
 	}
 	panic(ErrInvalidConvObjToNative)
 }
+
+func (ø Object) Cmp(v Val) int {
+	// First check for a custom Cmp method
+	if c, ok := ø["__cmp"]; ok {
+		return c.(*Func).Call(v) // TODO : Needs to setup `this`?
+	}
+
+	// If same reference as v, return 0 (equal)
+	if ø == v {
+		return 0
+	}
+	// Otherwise, return -1
+	return -1
+}
+
+// TODO : NotFound method, how does it work? See js proposition, Lua, others...
