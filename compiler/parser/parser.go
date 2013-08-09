859fbe3054310c595600b74055f692a4c9b21f79
Martin Angers <martin.n.angers@gmail.com>
2013-08-08 21:24:08 -0400

    more productions coded in the parser

--- compiler/parser/parser.go
+++ compiler/parser/parser.go
@@ -93,3 +93,94 @@ func (p *Parser) stmtList() bool {
 	}
 	return one
 }
+
+func (p *Parser) statement() bool {
+	return p.simpleStmt || p.returnStmt || p.breakStmt || p.continueStmt || p.ifStmt || p.forStmt
+}
+
+func (p *Parser) simpleStmt() bool {
+	// TODO : Later...
+}
+
+func (p *Parser) returnStmt() bool {
+	if p.match(token.RETURN) {
+		p.expression()
+		return true
+	}
+	return false
+}
+
+func (p *Parser) breakStmt() bool {
+	return p.match(token.BREAK)
+}
+
+func (p *Parser) continueStmt() bool {
+	return p.match(token.CONTINUE)
+}
+
+func (p *Parser) ifStmt() bool {
+	if p.match(token.IF) {
+		if !p.expression() {
+			p.Error("IfStmt : expected an expression")
+		}
+		if !p.block() {
+			p.Error("IfStmt : expected a body")
+		}
+		if p.match(token.ELSE) {
+			if !p.ifStmt() && !p.block() {
+				p.Error("IfStmt : expected an if or a body after the else keyword")
+			}
+		}
+		return true
+	}
+	return false
+}
+
+func (p *Parser) forStmt() bool {
+	if p.match(token.FOR) {
+		// For Clause must be checked first, range next, and condition last
+		if !(p.forClause() || p.rangeClause() || p.condition) {
+			p.Error("ForStmt : expected a condition, a for clause or a range clause")
+		}
+		if !p.block() {
+			p.Error("ForStmt : expected a for body")
+		}
+		return true
+	}
+	return false
+}
+
+func (p *Parser) condition() bool {
+	return p.expression()
+}
+
+func (p *Parser) forClause() bool {
+	p.initStmt() // Optional
+	if !p.match(token.SEMICOLON) {
+		// TODO : Rewind the initStmt!
+		return false
+	}
+	// Else, assume this is a for clause, the rest is expected
+	p.condition() // Optional
+	p.expect(token.SEMICOLON)
+	p.postStmt() // Optional
+	return true
+}
+
+func (p *Parser) rangeClause() bool {
+	var ok bool
+	if p.expressionList() {
+		ok = p.expect(token.ASSIGN)
+	} else if p.identifierList() {
+		ok = p.expect(token.DEFINE)
+	}
+	if !ok {
+		// TODO : Rewind?!?!?!
+		return false
+	}
+	p.expect(token.RANGE)
+	if !p.expression() {
+		p.Error("RangeClause : expected an expression")
+	}
+	return true
+}
