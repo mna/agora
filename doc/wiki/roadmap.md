4c3cd217e29249e94926746224f60546176bf141
Martin Angers <martin.n.angers@gmail.com>
2013-09-05 21:40:15 -0400

    add roadmap wiki article, math stdlib

---
+++ doc/wiki/roadmap.md
@@ -0,0 +1,45 @@
+The v0.1 release was a huge step to take this project off the ground, but it is also a **very** rough, **alpha** release. There is still a *lot* to do before even thinking using agora in a production-like environment, and before calling the language *stable*.
+
+Everything except the major goals of the language should be considered unstable, and everything could change. The native API is not frozen, the VM is not frozen, the compiler is definitely not frozen (thank god!) and even the syntax, runtime design and stdlib are not frozen. What *is* frozen is that this is and will remain a dynamically-typed, dynamically-loaded, garbage collected, embeddable language with a syntax close to Go.
+
+The following is by no means a rigid contract, more like an overly optimistic overview of the projected roadmap.
+
+## v0.2
+
+The next version will yet again focus on the language features and the runtime. The compiler could definitely use some work (and the command-line tool could be cleaned-up), but I don't feel this is a priority as long as the language is not yet stabilized. The compiler is *barely* good enough, but it *is* good enough for running and testing the language. It will probably only change when it is required for new features to be supported.
+
+The features envisioned for v0.2 are:
+
+* Decision regarding the `runtime.Int` type to represent an agora number. It will most likely go away, and only `runtime.Float` will remain, although a `runtime.Val` may still support conversion to `int`.
+* Change the generated instructions so that `&&` and `||` are short-circuiting.
+* Support the immediately-invoked function expression syntax (IIFE, i.e. `func(){}()`).
+* Support closures.
+* Support coroutines.
+* Using coroutines, support a flexible, extensible `for ... range` notation.
+* Better/more tests
+* Fix bugs
+
+This will probably be enough for this version.
+
+## v0.3
+
+The backlog will no doubt have time to fill up until then, but as it stands today, the next.next version will mostly be about the language and the runtime too. Expected features:
+
+* Support prototype/metatable inheritance, à-la Javascript or Lua.
+* Support a literal "array" notation (i.e. `a := [10, true, "hi"]`).
+* Optimize the object when used as an array (dense integer keys).
+* Support comma-separated list of assignments (for `:=` and `=`).
+* Introduce the `switch` statement.
+* Better/more tests
+* Fix bugs
+
+## Beyond
+
+Once the language features have landed, focus should turn to refactoring the compiler, returning better error messages (both in the compiler and the runtime), and stabilizing/enhancing the stdlib. Testing on Windows should also start somewhere around here, I don't have a Windows machine and I don't want to spend time debugging for an OS-specific problem while the project is evolving fast. As far as I can tell, at the moment at least, there is no OS-specific code in agora so it shouldn't be too hard to get it to work. The only thing that comes to mind is the directory separator.
+
+## Development
+
+Development will take place in the `next` branch. I will keep `master` stable and updated only with official releases (there *may* be v0.1.n releases before v0.2 lands, though, and if this happens, it will be on `master`).
+
+Next: [Design and architecture](https://github.com/PuerkitoBio/agora/wiki/Design-and-architecture)
+
---
+++ runtime/stdlib/math.go
@@ -0,0 +1,210 @@
+package stdlib
+
+import (
+	"math"
+	"math/rand"
+
+	"github.com/PuerkitoBio/agora/runtime"
+)
+
+type MathMod struct {
+	ctx *runtime.Ctx
+	ob  *runtime.Object
+}
+
+func (m *MathMod) ID() string {
+	return "math"
+}
+
+func (m *MathMod) Run(_ ...runtime.Val) (v runtime.Val, err error) {
+	defer runtime.PanicToError(&err)
+	if m.ob == nil {
+		// Prepare the object
+		m.ob = runtime.NewObject()
+		m.ob.Set(runtime.String("Pi"), runtime.Float(math.Pi))
+		m.ob.Set(runtime.String("Abs"), runtime.NewNativeFunc(m.ctx, "math.Abs", m.math_Abs))
+		m.ob.Set(runtime.String("Acos"), runtime.NewNativeFunc(m.ctx, "math.Acos", m.math_Acos))
+		m.ob.Set(runtime.String("Acosh"), runtime.NewNativeFunc(m.ctx, "math.Acosh", m.math_Acosh))
+		m.ob.Set(runtime.String("Asin"), runtime.NewNativeFunc(m.ctx, "math.Asin", m.math_Asin))
+		m.ob.Set(runtime.String("Asinh"), runtime.NewNativeFunc(m.ctx, "math.Asinh", m.math_Asinh))
+		m.ob.Set(runtime.String("Atan"), runtime.NewNativeFunc(m.ctx, "math.Atan", m.math_Atan))
+		m.ob.Set(runtime.String("Atan2"), runtime.NewNativeFunc(m.ctx, "math.Atan2", m.math_Atan2))
+		m.ob.Set(runtime.String("Atanh"), runtime.NewNativeFunc(m.ctx, "math.Atanh", m.math_Atanh))
+		m.ob.Set(runtime.String("Ceil"), runtime.NewNativeFunc(m.ctx, "math.Ceil", m.math_Ceil))
+		m.ob.Set(runtime.String("Cos"), runtime.NewNativeFunc(m.ctx, "math.Cos", m.math_Cos))
+		m.ob.Set(runtime.String("Cosh"), runtime.NewNativeFunc(m.ctx, "math.Cosh", m.math_Cosh))
+		m.ob.Set(runtime.String("Exp"), runtime.NewNativeFunc(m.ctx, "math.Exp", m.math_Exp))
+		m.ob.Set(runtime.String("Floor"), runtime.NewNativeFunc(m.ctx, "math.Floor", m.math_Floor))
+		m.ob.Set(runtime.String("Inf"), runtime.NewNativeFunc(m.ctx, "math.Inf", m.math_Inf))
+		m.ob.Set(runtime.String("IsInf"), runtime.NewNativeFunc(m.ctx, "math.IsInf", m.math_IsInf))
+		m.ob.Set(runtime.String("IsNaN"), runtime.NewNativeFunc(m.ctx, "math.IsNaN", m.math_IsNaN))
+		m.ob.Set(runtime.String("Max"), runtime.NewNativeFunc(m.ctx, "math.Max", m.math_Max))
+		m.ob.Set(runtime.String("Min"), runtime.NewNativeFunc(m.ctx, "math.Min", m.math_Min))
+		m.ob.Set(runtime.String("NaN"), runtime.NewNativeFunc(m.ctx, "math.NaN", m.math_NaN))
+		m.ob.Set(runtime.String("Pow"), runtime.NewNativeFunc(m.ctx, "math.Pow", m.math_Pow))
+		m.ob.Set(runtime.String("Sin"), runtime.NewNativeFunc(m.ctx, "math.Sin", m.math_Sin))
+		m.ob.Set(runtime.String("Sinh"), runtime.NewNativeFunc(m.ctx, "math.Sinh", m.math_Sinh))
+		m.ob.Set(runtime.String("Sqrt"), runtime.NewNativeFunc(m.ctx, "math.Sqrt", m.math_Sqrt))
+		m.ob.Set(runtime.String("Tan"), runtime.NewNativeFunc(m.ctx, "math.Tan", m.math_Tan))
+		m.ob.Set(runtime.String("Tanh"), runtime.NewNativeFunc(m.ctx, "math.Tanh", m.math_Tanh))
+		m.ob.Set(runtime.String("RandSeed"), runtime.NewNativeFunc(m.ctx, "math.RandSeed", m.math_RandSeed))
+		m.ob.Set(runtime.String("Rand"), runtime.NewNativeFunc(m.ctx, "math.Rand", m.math_Rand))
+	}
+	return m.ob, nil
+}
+
+func (m *MathMod) SetCtx(ctx *runtime.Ctx) {
+	m.ctx = ctx
+}
+
+func (m *MathMod) math_Abs(args ...runtime.Val) runtime.Val {
+	runtime.ExpectAtLeastNArgs(1, args)
+	return runtime.Float(math.Abs(args[0].Float()))
+}
+
+func (m *MathMod) math_Acos(args ...runtime.Val) runtime.Val {
+	runtime.ExpectAtLeastNArgs(1, args)
+	return runtime.Float(math.Acos(args[0].Float()))
+}
+
+func (m *MathMod) math_Acosh(args ...runtime.Val) runtime.Val {
+	runtime.ExpectAtLeastNArgs(1, args)
+	return runtime.Float(math.Acosh(args[0].Float()))
+}
+
+func (m *MathMod) math_Asin(args ...runtime.Val) runtime.Val {
+	runtime.ExpectAtLeastNArgs(1, args)
+	return runtime.Float(math.Asin(args[0].Float()))
+}
+
+func (m *MathMod) math_Asinh(args ...runtime.Val) runtime.Val {
+	runtime.ExpectAtLeastNArgs(1, args)
+	return runtime.Float(math.Asinh(args[0].Float()))
+}
+
+func (m *MathMod) math_Atan(args ...runtime.Val) runtime.Val {
+	runtime.ExpectAtLeastNArgs(1, args)
+	return runtime.Float(math.Atan(args[0].Float()))
+}
+
+func (m *MathMod) math_Atan2(args ...runtime.Val) runtime.Val {
+	runtime.ExpectAtLeastNArgs(2, args)
+	return runtime.Float(math.Atan2(args[0].Float(), args[1].Float()))
+}
+
+func (m *MathMod) math_Atanh(args ...runtime.Val) runtime.Val {
+	runtime.ExpectAtLeastNArgs(1, args)
+	return runtime.Float(math.Atanh(args[0].Float()))
+}
+
+func (m *MathMod) math_Ceil(args ...runtime.Val) runtime.Val {
+	runtime.ExpectAtLeastNArgs(1, args)
+	return runtime.Float(math.Ceil(args[0].Float()))
+}
+
+func (m *MathMod) math_Cos(args ...runtime.Val) runtime.Val {
+	runtime.ExpectAtLeastNArgs(1, args)
+	return runtime.Float(math.Cos(args[0].Float()))
+}
+
+func (m *MathMod) math_Cosh(args ...runtime.Val) runtime.Val {
+	runtime.ExpectAtLeastNArgs(1, args)
+	return runtime.Float(math.Cosh(args[0].Float()))
+}
+
+func (m *MathMod) math_Exp(args ...runtime.Val) runtime.Val {
+	runtime.ExpectAtLeastNArgs(1, args)
+	return runtime.Float(math.Exp(args[0].Float()))
+}
+
+func (m *MathMod) math_Floor(args ...runtime.Val) runtime.Val {
+	runtime.ExpectAtLeastNArgs(1, args)
+	return runtime.Float(math.Floor(args[0].Float()))
+}
+
+func (m *MathMod) math_Inf(args ...runtime.Val) runtime.Val {
+	runtime.ExpectAtLeastNArgs(1, args)
+	return runtime.Float(math.Inf(int(args[0].Int())))
+}
+
+func (m *MathMod) math_IsInf(args ...runtime.Val) runtime.Val {
+	runtime.ExpectAtLeastNArgs(2, args)
+	return runtime.Bool(math.IsInf(args[0].Float(), int(args[1].Int())))
+}
+
+func (m *MathMod) math_IsNaN(args ...runtime.Val) runtime.Val {
+	runtime.ExpectAtLeastNArgs(1, args)
+	return runtime.Bool(math.IsNaN(args[0].Float()))
+}
+
+func (m *MathMod) math_Max(args ...runtime.Val) runtime.Val {
+	runtime.ExpectAtLeastNArgs(2, args)
+	max := args[len(args)-1].Float()
+	for i := len(args) - 2; i >= 0; i-- {
+		max = math.Max(max, args[i].Float())
+	}
+	return runtime.Float(max)
+}
+
+func (m *MathMod) math_Min(args ...runtime.Val) runtime.Val {
+	runtime.ExpectAtLeastNArgs(2, args)
+	min := args[len(args)-1].Float()
+	for i := len(args) - 2; i >= 0; i-- {
+		min = math.Min(min, args[i].Float())
+	}
+	return runtime.Float(min)
+}
+
+func (m *MathMod) math_NaN(_ ...runtime.Val) runtime.Val {
+	return runtime.Float(math.NaN())
+}
+
+func (m *MathMod) math_Pow(args ...runtime.Val) runtime.Val {
+	runtime.ExpectAtLeastNArgs(2, args)
+	return runtime.Float(math.Pow(args[0].Float(), args[1].Float()))
+}
+
+func (m *MathMod) math_Sin(args ...runtime.Val) runtime.Val {
+	runtime.ExpectAtLeastNArgs(1, args)
+	return runtime.Float(math.Sin(args[0].Float()))
+}
+
+func (m *MathMod) math_Sinh(args ...runtime.Val) runtime.Val {
+	runtime.ExpectAtLeastNArgs(1, args)
+	return runtime.Float(math.Sinh(args[0].Float()))
+}
+
+func (m *MathMod) math_Sqrt(args ...runtime.Val) runtime.Val {
+	runtime.ExpectAtLeastNArgs(1, args)
+	return runtime.Float(math.Sqrt(args[0].Float()))
+}
+
+func (m *MathMod) math_Tan(args ...runtime.Val) runtime.Val {
+	runtime.ExpectAtLeastNArgs(1, args)
+	return runtime.Float(math.Tan(args[0].Float()))
+}
+
+func (m *MathMod) math_Tanh(args ...runtime.Val) runtime.Val {
+	runtime.ExpectAtLeastNArgs(1, args)
+	return runtime.Float(math.Tanh(args[0].Float()))
+}
+
+func (m *MathMod) math_RandSeed(args ...runtime.Val) runtime.Val {
+	runtime.ExpectAtLeastNArgs(1, args)
+	rand.Seed(args[0].Int())
+	return runtime.Nil
+}
+
+func (m *MathMod) math_Rand(args ...runtime.Val) runtime.Val {
+	switch len(args) {
+	case 0:
+		return runtime.Int(rand.Int())
+	case 1:
+		return runtime.Int(rand.Intn(int(args[0].Int())))
+	default:
+		low := args[0].Int()
+		high := args[1].Int()
+		n := rand.Intn(int(high - low))
+		return runtime.Int(int64(n) + low)
+	}
+}
