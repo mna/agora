8701d7f97e0b78d606b0df6daf531e9ee602a69e
Martin Angers <martin.n.angers@gmail.com>
2013-09-16 22:18:44 -0400

    make build

--- bytecode/build.go
+++ bytecode/build.go
@@ -1,5 +1,5 @@
 package bytecode
 const (
-	AGORA_BUILD = "f19ec35"
-	GO_BUILD = "go version devel +a6d1a3f0411a Sat Sep 14 09:30:36 2013 +0200 darwin/amd64"
+	AGORA_BUILD = "3c9c464"
+	GO_BUILD = "go version devel +c111e30c49a4 Mon Sep 16 20:31:21 2013 -0400 linux/amd64"
 )
